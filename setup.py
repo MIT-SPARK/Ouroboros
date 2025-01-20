18fe232097ec1561dd941258242bb37910b21d92
Nathan Hughes <nathan.h.hughes@gmail.com>
2025-01-20 00:15:39 +0000

    prototype initial opengv bindings infrastructure

--- setup.py
+++ setup.py
@@ -1,4 +1,28 @@
+import subprocess
+
+from pybind11.setup_helpers import Pybind11Extension
 from setuptools import find_packages, setup
+
+
+def _get_flags(name):
+    return (
+        subprocess.run(["pkg-config", "--cflags", name], capture_output=True)
+        .stdout.decode("utf-8")
+        .strip("\n")
+        .split(" ")
+    )
+
+
+ext_modules = [
+    Pybind11Extension(
+        "_ouroboros_opengv",
+        [
+            "src/ouroboros-opengv/_bindings.cpp",
+            "third_party/opengv/src/relative_pose/CentralRelativeAdapter.cpp",
+        ],
+        extra_compile_args=["-Ithird_party/opengv/include"] + _get_flags("eigen3"),
+    )
+]
 
 setup(
     name="ouroboros",
@@ -13,11 +37,11 @@ setup(
     install_requires=[
         "numpy",
         "imageio",
-        "pyyaml",
-        "pytest",
-        "importlib-resources",
-        "scipy",
         "matplotlib",
         "opencv-python",
+        "pyyaml",
+        "pytest",
+        "scipy",
     ],
+    ext_modules=ext_modules,
 )
