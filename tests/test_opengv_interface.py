678a2a101fc45c08e0c097064a7d0dbc8c5b4da7
Nathan Hughes <nathan.h.hughes@gmail.com>
2025-01-30 20:02:15 +0000

    (wip) start on full pose recovery test

--- tests/test_opengv_interface.py
+++ tests/test_opengv_interface.py
@@ -7,45 +7,32 @@ import ouroboros as ob
 import ouroboros_opengv as ogv
 
 
+def _get_test_pose():
+    angle = np.pi / 4.0
+    match_R_query = np.array([np.sin(angle / 2), 0.0, 0.0, np.cos(angle / 2)])
+    match_t_query = np.array([0.3, -0.2, 0.1]).reshape((3, 1))
+    return ob.pose_from_quat_trans(match_R_query, match_t_query)
+
+
 def _transform_points(bearings, dest_T_src):
     return bearings @ dest_T_src[:3, :3].T + dest_T_src[:3, 3, np.newaxis].T
 
 
-def _inverse_pose(dest_T_src):
-    src_T_dest = np.eye(4)
-    src_T_dest[:3, :3] = dest_T_src[:3, :3].T
-    src_T_dest[:3, 3] = -dest_T_src[:3, :3].T @ dest_T_src[:3, 3]
-    return src_T_dest
+def test_nonmetric_solver():
+    """Test that two-view geometry is called correctly."""
+    rng = np.random.default_rng(0)
 
+    expected = _get_test_pose()  # match_T_query
+    query_features = rng.normal(size=(100, 2))
+    query_bearings, _ = ob.get_bearings(np.eye(3), query_features)
+    match_bearings = _transform_points(query_bearings, expected)
 
-def _pose(dest_R_src, dest_t_src):
-    dest_T_src = np.eye(4)
-    dest_T_src[:3, :3] = dest_R_src
-    dest_T_src[:3, 3] = np.squeeze(dest_t_src)
-    return dest_T_src
+    result = ogv.solve_2d2d(
+        query_bearings.T,
+        match_bearings.T,
+        solver=ogv.Solver2d2d.NISTER,
+    ).dest_T_src
 
-
-def _get_test_pose():
-    yaw = np.pi / 4.0
-    match_R_query = np.array(
-        [
-            [1.0, 0.0, 0.0],
-            [0.0, np.cos(yaw), -np.sin(yaw)],
-            [0.0, np.sin(yaw), np.cos(yaw)],
-        ]
-    )
-    # NOTE(nathan) needs to be small to avoid chirality inversion
-    match_t_query = np.array([0.3, -0.2, 0.1]).reshape((3, 1))
-    return _pose(match_R_query, match_t_query)
-
-
-def _shuffle_features(features):
-    indices = np.arange(features.shape[0])
-    np.random.shuffle(indices)
-    return indices, features[indices, :].copy()
-
-
-def _check_pose_up_to_scale(expected, result):
     t_expected = expected[:3, 3]
     t_expected = t_expected / np.linalg.norm(t_expected)
     t_result = result[:3, 3]
@@ -54,45 +41,34 @@ def _check_pose_up_to_scale(expected, result):
     assert t_result == pytest.approx(t_expected, abs=1.0e-3)
 
 
-def test_nonmetric_solver():
-    """Test that two-view geometry is called correctly."""
+def test_pose_recovery():
+    """Test pose recovery works as expected."""
+    rng = np.random.default_rng(0)
     expected = _get_test_pose()  # match_T_query
-    query_features = np.random.normal(size=(100, 2))
-    query_bearings, _ = ob.get_bearings(np.eye(3), query_features)
-    match_bearings = _transform_points(query_bearings, expected)
 
-    result = ogv.solve_2d2d(
-        query_bearings.T,
-        match_bearings.T,
-        solver=ogv.Solver2d2d.NISTER,
-    )
+    match = ob.VlcImage(None, ob.SparkImage())
+    match.keypoints = rng.normal(size=(30, 2))
+    match_depths = rng.uniform(1.5, 2.5, match.keypoints.shape[0])
+    _, match_points = ob.get_bearings(np.eye(3), match.keypoints, match_depths)
 
-    _check_pose_up_to_scale(expected, result.dest_T_src)
+    query = ob.VlcImage(None, ob.SparkImage())
+    query_points = _transform_points(match_points, ob.invert_pose(expected))
+    query.keypoints = query_points[:, :2] / query_points[:, 2, np.newaxis]
 
+    indices = np.arange(query_points.shape[0])
+    new_indices = np.arange(query.keypoints.shape[0])
+    rng.shuffle(new_indices)
+    query.keypoints = query.keypoints[new_indices, :].copy()
+    # query_depths = query_points[new_indices, 2]
 
-# def test_metric_solver():
-# """Test that 2d-3d solver is called correctly."""
-# expected = _get_test_pose()  # match_T_query
+    # needs to be query -> match (so need indices that were used by shuffle for match)
+    correspondences = np.vstack((indices, new_indices)).T
 
-# match_features = np.random.normal(size=(30, 2))
-# match_depths = np.random.uniform(1.5, 2.5, match_features.shape[0])
-# match_points = ogv.get_points(np.eye(3), match_features, match_depths)
+    config = ogv.OpenGVPoseRecoveryConfig()
+    pose_recovery = ogv.OpenGVPoseRecovery(config)
+    vlc_db = ob.VlcDb(10)
+    result = pose_recovery.recover_pose(vlc_db, match, query, correspondences)
 
-# query_points = _transform_points(match_points, _inverse_pose(expected))
-# query_features = query_points[:, :2] / query_points[:, 2, np.newaxis]
-
-# indices = np.arange(query_points.shape[0])
-# new_indices, query_features = _shuffle_features(query_features)
-
-# # needs to be query -> match (so need indices that were used by shuffle for match)
-# correspondences = np.vstack((indices, new_indices)).T
-# result = ogv.recover_metric_pose_opengv(
-# np.eye(3),
-# query_features,
-# np.eye(3),
-# match_features,
-# match_depths,
-# correspondences,
-# solver=ogv.Solver2d2d.STEWENIUS,
-# )
-# assert result == pytest.approx(expected)
+    assert result
+    assert result.is_metric
+    assert result.query_T_match == pytest.approx(expected)
