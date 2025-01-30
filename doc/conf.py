b7a1d8f62321e7a579561e0d8fce6bc81314ba9b
Nathan Hughes <nathan.h.hughes@gmail.com>
2025-01-30 00:04:58 +0000

    add sphinx setup

---
+++ doc/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Ouroboros"
+copyright = "2025, MIT-SPARK Lab, Massachusetts Institute of Technology"
+author = "Aaron Ray, Mohammed Ehab, Nathan Hughes, Toya Takahashi, Yun Chang"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Docstring configuration -------------------------------------------------
+maximum_signature_line_length = 88
+autodoc_preserve_defaults = False
+autodoc_typehints = "description"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
+html_theme_options = {
+    "font_size": "12pt",
+}
