45a94303d98f23e429fd6a1e3d6e7f1529b7b554
Yun Chang <yunchang@mit.edu>
2025-02-12 17:02:32 -0500

    start setting up vlc msgs

--- extra/ouroboros_ros/src/ouroboros_ros/utils.py
+++ extra/ouroboros_ros/src/ouroboros_ros/utils.py
@@ -1,8 +1,35 @@
 import numpy as np
 import rospy
 import tf2_ros
+from cv_bridge import CvBridge
+from ouroboros_msgs.msg import SparkImageMsg, VlcImageMetadataMsg, VlcImageMsg
 
-from ouroboros import VlcPose
+from ouroboros import SparkImage, VlcImage, VlcImageMetadata, VlcPose
+
+
+def vlc_image_metadata_from_msg(metadata_msg: VlcImageMetadataMsg):
+    return VlcImageMetadata(
+        image_uuid=metadata_msg.uuid,
+        session_id=metadata_msg.session_id,
+        epoch_ns=metadata_msg.epoch_ns,
+    )
+
+
+def spark_image_from_msg(image_msg: SparkImageMsg):
+    bridge = CvBridge()
+    return SparkImage(
+        rgb=bridge.imgmsg_to_cv2(image_msg.rgb, desired_encoding="passthrough"),
+        depth=bridge.imgmsg_to_cv2(image_msg.depth, desired_encoding="passthrough"),
+    )
+
+
+def vlc_image_from_msg(vlc_msg: VlcImageMsg):
+    vlc_image = VlcImage(
+        metadata=vlc_image_metadata_from_msg(vlc_msg.metadata),
+        image=spark_image_from_msg(vlc_msg.metadata),
+    )
+
+    return vlc_image
 
 
 def get_tf_as_pose(tf_buffer, fixed_frame, body_frame, time=None):
