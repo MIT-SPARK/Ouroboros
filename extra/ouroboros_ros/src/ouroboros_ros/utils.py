8501aa114b1d0445fb513a41709f8fd4895d0121
Nathan Hughes <nathan.h.hughes@gmail.com>
2025-02-22 20:46:19 +0000

    handle failed plugin loading and forward logging to ros

--- extra/ouroboros_ros/src/ouroboros_ros/utils.py
+++ extra/ouroboros_ros/src/ouroboros_ros/utils.py
@@ -1,3 +1,5 @@
+import logging
+
 import numpy as np
 import rospy
 import tf2_ros
@@ -107,3 +109,28 @@ def parse_camera_info(info_msg):
     cx = K[0, 2]
     cy = K[1, 2]
     return ob.PinholeCamera(fx=fx, fy=fy, cx=cx, cy=cy)
+
+
+# adapted from https://gist.github.com/ablakey/4f57dca4ea75ed29c49ff00edf622b38
+class RosForwarder(logging.Handler):
+    """Class to forward logging to ros handler."""
+
+    level_map = {
+        logging.DEBUG: rospy.logdebug,
+        logging.INFO: rospy.loginfo,
+        logging.WARNING: rospy.logwarn,
+        logging.ERROR: rospy.logerr,
+        logging.CRITICAL: rospy.logfatal,
+    }
+
+    def emit(self, record):
+        """Send message to ROS."""
+        level = record.levelno if record.levelno in self.level_map else logging.CRITICAL
+        self.level_map[level](f"{record.name}: {record.msg}")
+
+
+def setup_ros_log_forwarding(level=logging.INFO):
+    """Forward logging to ROS."""
+    logger = logging.getLogger("ouroboros")
+    logger.addHandler(RosForwarder())
+    logger.setLevel(level)
--- src/ouroboros/__init__.py
+++ src/ouroboros/__init__.py
@@ -1,4 +1,5 @@
 import importlib
+import logging
 import pkgutil
 
 from ouroboros.config import *
@@ -21,11 +22,21 @@ from ouroboros.vlc_server.vlc_server import VlcServer, VlcServerConfig
 
 
 def discover_plugins():
+    def _try_load(name):
+        try:
+            return importlib.import_module(name)
+        except ImportError as e:
+            logging.getLogger("ouroboros").warning(
+                f"Unable to load plugin '{name}': {e}"
+            )
+            return None
+
     discovered_plugins = {
-        name: importlib.import_module(name)
+        name: _try_load(name)
         for finder, name, ispkg in pkgutil.iter_modules()
         if name.startswith("ouroboros_")
     }
+    discovered_plugins = {k: v for k, v in discovered_plugins.items() if v is not None}
     return discovered_plugins
 
 
