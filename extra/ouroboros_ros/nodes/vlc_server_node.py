0c6777afd898b46853b6db304fa8c087be517087
Nathan Hughes <nathan.h.hughes@gmail.com>
2025-02-22 20:46:19 +0000

    handle failed plugin loading and forward logging to ros

--- extra/ouroboros_ros/nodes/vlc_server_node.py
+++ extra/ouroboros_ros/nodes/vlc_server_node.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 import rospy
+from ouroboros_ros.utils import setup_ros_log_forwarding
 from ouroboros_ros.vlc_server_ros import VlcServerRos
 
 if __name__ == "__main__":
     rospy.init_node("vlc_server_node")
+    setup_ros_log_forwarding()
     node = VlcServerRos()
     node.run()
